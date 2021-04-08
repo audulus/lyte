13732d6065fc3a290e6b87582dd3f27b931115b4
Taylor Holliday <taylor@audulus.com>
2021-04-08 14:16:25 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,47 @@
 
-use std::rc::Rc;
+// use std::rc::Rc;
 // use std::collections::HashSet;
 
-#[derive(Hash, Eq, PartialEq, Debug)]
+#[derive(Clone, Hash, Eq, PartialEq, Debug)]
 pub enum Type {
 	Void,
 	Int8,
 	Int32,
-	Tuple(Rc<Type>, Rc<Type>)
+	Tuple(Box<Type>, Box<Type>),
+    Var(usize),
+    Function(Box<Type>, Box<Type>)
 }
 
-pub struct TypeFactory {
-    types: Vec<Rc<Type>>,
-}
+pub type Instance = Vec<Type>;
 
-impl TypeFactory {
-    pub fn new() -> TypeFactory {
-        TypeFactory { types: [].to_vec() }
+pub fn subst(t: Type, inst: &Instance) -> Type {
+    match t {
+        Type::Tuple(a, b) => Type::Tuple(Box::new(subst(*a, inst)), Box::new(subst(*b, inst))),
+        Type::Var(i) => inst[i].clone(),
+        _ => t
     }
+}
 
-    pub fn unique(&mut self, t: Type) -> Rc<Type> {
-        for p in &self.types {
-            if **p == t {
-                return p.clone();
-            }
+pub fn unify(lhs: Type, rhs: Type, inst: &mut Instance) -> bool {
+    if lhs == rhs {
+        true
+    } else {
+        match (lhs, rhs) {
+            (Type::Tuple(a, b), Type::Tuple(c, d)) => unify(*a, *c, inst) && unify(*b, *d, inst),
+            (Type::Var(i), rhs) => { inst[i] = rhs; true },
+            _ => return false
         }
-        let p = Rc::new(t);
-        self.types.push(p.clone());
-        return p
-    }
-
-    pub fn tuple(&mut self, a: Rc<Type>, b: Rc<Type>) -> Rc<Type> {
-        self.unique(Type::Tuple(a, b))
     }
 }
 
 fn main() {
-    let mut f = TypeFactory::new();
+    assert_eq!(Type::Void, Type::Void);
+    assert_ne!(Type::Void, Type::Int8);
 
-    let t0 = f.unique(Type::Void);
-    let t1 = f.unique(Type::Void);
+    let mut inst = vec![Type::Void];
+    assert!(unify(Type::Void, Type::Void, &mut inst));
+    assert!(!unify(Type::Void, Type::Int8, &mut inst));
 
-    assert_eq!(t0, t1);
-
-    let t2 = f.unique(Type::Int8);
-
-    assert_ne!(t1, t2);
-
-    let t3 = f.tuple(t2.clone(), t2.clone());
-    let t4 = f.tuple(t2.clone(), t2.clone());
-
-    assert_eq!(t3, t4);
-
-    let t5 = f.unique(Type::Tuple(t2.clone(), t0.clone()));
-
-    assert_ne!(t3, t5);
+    assert!(unify(Type::Var(0), Type::Int8, &mut inst));
+    assert_eq!(inst[0], Type::Int8);
 }
