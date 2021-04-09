87f85a53356049a4e74abc2c423d782fef8f0e2c
Taylor Holliday <taylor@audulus.com>
2021-04-08 22:33:02 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,6 @@
 
 pub mod ast;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ast::{unify, Type};
-
-    #[test]
-    fn test_basic_types() {
-        assert_eq!(Type::Void, Type::Void);
-        assert_ne!(Type::Void, Type::Int8);
-    }
-
-    #[test]
-    fn test_tuple() {
-        assert_eq!(Type::Tuple(Box::new(Type::Void), Box::new(Type::Void)), Type::Tuple(Box::new(Type::Void), Box::new(Type::Void)));
-    }
-
-    #[test]
-    fn test_unify() {
-        let mut inst = vec![Type::Void];
-        assert!(unify(Type::Void, Type::Void, &mut inst));
-        assert!(!unify(Type::Void, Type::Int8, &mut inst));
-    
-        assert!(unify(Type::Var(0), Type::Int8, &mut inst));
-        assert_eq!(inst[0], Type::Int8);
-    }
-}
-
 fn main() {
     println!("yo")
 }
