ff29c7e28d0203b824d5d46e058d167075e86f48
Taylor Holliday <taylor@audulus.com>
2021-05-30 15:25:32 -0700

    type safety

--- src/ast.rs
+++ src/ast.rs
@@ -4,41 +4,46 @@ use std::hash::{Hash, Hasher};
 use std::rc::Rc;
 
 #[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
+pub struct TypeID {
+    index: u32
+}
+
+#[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
 pub enum Type {
     Void,
     Int8,
     Int32,
-    Tuple(u32, u32),
+    Tuple(TypeID, TypeID),
     Var(u32),
-    Func(u32, u32),
+    Func(TypeID, TypeID),
 }
 
 pub struct Compiler {
     types: Vec<Type>,
 }
 
-pub type Instance = HashMap<u32, u32>;
+pub type Instance = HashMap<u32, TypeID>;
 
 impl Compiler {
     pub fn new() -> Compiler {
         Compiler { types: Vec::new() }
     }
 
-    pub fn mk_type(&mut self, proto: Type) -> u32 {
+    pub fn mk_type(&mut self, proto: Type) -> TypeID {
         // Dumb linear search.
         for i in 0..self.types.len() {
             if self.types[i] == proto {
-                return i as u32;
+                return TypeID{index: i as u32};
             }
         }
 
         let ix = self.types.len();
         self.types.push(proto);
-        return ix as u32;
+        return TypeID{index: ix as u32};
     }
 
-    pub fn subst(&mut self, t: u32, inst: &Instance) -> u32 {
-        match self.types[t as usize] {
+    pub fn subst(&mut self, t: TypeID, inst: &Instance) -> TypeID {
+        match self.types[t.index as usize] {
             Type::Tuple(a, b) => {
                 let nt = Type::Tuple(self.subst(a, inst), self.subst(b, inst));
                 self.mk_type(nt)
@@ -51,8 +56,8 @@ impl Compiler {
         }
     }
 
-    pub fn solved(&self, t: u32) -> bool {
-        match self.types[t as usize] {
+    pub fn solved(&self, t: TypeID) -> bool {
+        match self.types[t.index as usize] {
             Type::Tuple(a, b) => self.solved(a) && self.solved(b),
             Type::Func(a, b) => self.solved(a) && self.solved(b),
             Type::Var(_) => false,
@@ -60,11 +65,11 @@ impl Compiler {
         }
     }
 
-    pub fn unify(&self, lhs: u32, rhs: u32, inst: &mut Instance) -> bool {
+    pub fn unify(&self, lhs: TypeID, rhs: TypeID, inst: &mut Instance) -> bool {
         if lhs == rhs {
             true
         } else {
-            match (self.types[lhs as usize], self.types[rhs as usize]) {
+            match (self.types[lhs.index as usize], self.types[rhs.index as usize]) {
                 (Type::Tuple(a, b), Type::Tuple(c, d)) => {
                     self.unify(a, c, inst) && self.unify(b, d, inst)
                 }
