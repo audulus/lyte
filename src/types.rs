023a9c0fd54d7f0077986b9f779c122433447250
Taylor Holliday <taylor@audulus.com>
2021-05-31 21:58:51 -0700

    fix warnings

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
-use std::collections::hash_map::DefaultHasher;
+//use std::collections::hash_map::DefaultHasher;
 use std::collections::{HashMap, HashSet};
 use std::hash::{Hash, Hasher};
-use std::rc::Rc;
+//use std::rc::Rc;
 
 #[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
 pub struct TypeID {
