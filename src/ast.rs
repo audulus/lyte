b80ccd7f7e21b1634102240704e099e2c2f56a70
Taylor Holliday <taylor@audulus.com>
2021-06-02 07:20:39 -0700

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,33 @@
+use crate::types::TypeID;
+
 #[derive(Clone, Hash, Eq, PartialEq, Debug)]
 pub struct Loc {
     pub file: String,
     pub line: u32,
+}
+
+pub enum ExprEnum {
+    Id(String),
+    Int(i64),
+    Real(f64),
+    Call(Box<Expr>, Box<Expr>),
+    Binop(Box<Expr>, Box<Expr>),
+    Unop(Box<Expr>),
+    Lambda,
+    String(String),
+    Char(u8),
+    Subscript,
+    Cast,
+    Field,
+    Array,
+    New,
+    True,
+    False
+}
+
+pub struct Expr {
+    pub expr: ExprEnum,
+    pub loc: Loc,
+    pub t: TypeID,
+    pub lvalue: bool
 }
