7ee7d9505d6116cca0bd397bd5400a480b83502e
Taylor Holliday <taylor@audulus.com>
2022-01-08 21:37:48 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #![allow(dead_code)]
 
 mod defs;
-use defs::*;
-
+//use defs::*;
 mod types;
-use types::*;
+//use types::*;
 mod typegraph;
 // use typegraph::*;
 
@@ -13,10 +12,6 @@ mod pest_parser;
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
-
-use internment::Intern;
-
-use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "lyte.pest"]
