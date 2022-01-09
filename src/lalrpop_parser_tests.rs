adcfe99613d8cf56db21ea2ecaec8d90e6ebab37
Taylor Holliday <taylor@audulus.com>
2022-01-09 09:41:57 -0800

    work

--- src/lalrpop_parser_tests.rs
+++ src/lalrpop_parser_tests.rs
@@ -6,5 +6,5 @@ lalrpop_mod!(pub lyte); // synthesized by LALRPOP
 #[test]
 fn basic() {
     assert!(lyte::TyParser::new().parse("i8").is_ok());
-    assert!(lyte::TyParser::new().parse("foo").is_err());
+    assert!(lyte::TyParser::new().parse("foo").is_ok());
 }
