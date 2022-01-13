3bb3b4bf90a348250ba94293fd9bbe199701e845
Taylor Holliday <taylor@audulus.com>
2022-01-13 09:51:35 -0800

    Update nom_parser.rs

--- src/nom_parser.rs
+++ src/nom_parser.rs
@@ -16,7 +16,7 @@ use nom::{
 
 // https://github.com/Geal/nom/blob/main/doc/choosing_a_combinator.md
 
-pub fn identifier(input: &str) -> IResult<&str, &str> {
+fn identifier(input: &str) -> IResult<&str, &str> {
     recognize(pair(
         alt((alpha1, tag("_"))),
         many0(alt((alphanumeric1, tag("_")))),
@@ -27,7 +27,7 @@ fn ty_from_id(_input: &str) -> Result<TypeID, std::num::ParseIntError> {
     Ok(mk_type(Type::Int32))
 }
 
-pub fn typevar(input: &str) -> IResult<&str, TypeID> {
+fn typevar(input: &str) -> IResult<&str, TypeID> {
     map_res(
         delimited(tag("⟨"), identifier, tag("⟩")),
         ty_from_id
