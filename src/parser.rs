53e2b475da580651e44db2316bbf8617b162763b
Taylor Holliday <taylor@audulus.com>
2022-01-15 17:14:02 -0800

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -3,18 +3,37 @@ use crate::defs::*;
 use crate::lexer::*;
 use crate::types::*;
 
-fn parse_basic_type(lexer: &mut Lexer) -> TypeID {
+#[derive(Clone, Debug)]
+struct ParseError {
+    location: usize,
+    message: String
+}
 
-    mk_type(match lexer.tok {
+fn expect(lexer: &Lexer, tok: Token) -> Result<(), ParseError> {
+    if lexer.tok == tok { Ok(()) } else {
+        let message = format!("expected {:?}, got {:?}", tok, lexer.tok);
+        println!("{:?}", message);
+        Err(ParseError{location: lexer.i, message: message})
+    }
+}
+
+fn parse_basic_type(lexer: &mut Lexer) -> Result<TypeID, ParseError> {
+
+    Ok(mk_type(match lexer.tok {
         Token::Void => Type::Void,
         Token::Int8 => Type::Int8,
         Token::Int32 => Type::Int32,
         Token::Lmath => {
 
+            lexer.next();
+
+            lexer.next();
+
+            expect(lexer, Token::Rmath)?;
             Type::Var(0)
         },
         _ => unreachable!()
-    })
+    }))
 }
 
 #[cfg(test)]
@@ -25,7 +44,7 @@ mod tests {
     fn type_parser(string: &str) -> TypeID {
         let mut lexer = Lexer::new(&String::from(string));
         lexer.next();
-        parse_basic_type(&mut lexer)
+        parse_basic_type(&mut lexer).unwrap()
     }
 
     #[test]
