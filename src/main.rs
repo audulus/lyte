8ae39458e3cd3dc8abd092dddc29d0b2541ed47b
Taylor Holliday <wtholliday@gmail.com>
2022-05-04 22:12:59 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod parser;
 
 use clap::Parser;
 use std::fs;
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -24,17 +25,31 @@ impl Compiler {
     pub fn new() -> Self {
         Self { names: vec![] }
     }
+
+    pub fn parse_file(&mut self, path: &Path) {
+        if let Ok(string) = fs::read_to_string(path) {
+            println!("parsing file: {:?}", path);
+            let mut lexer = Lexer::new(&String::from(string));
+            lexer.next();
+        } else {
+            println!("error reading file: {:?}", path);
+        }
+    }
 }
 
 fn main() {
     println!("🎸");
 
     let args = Args::parse();
+    let mut compiler = Compiler::new();
 
-    if let Ok(string) = fs::read_to_string(args.file) {
-        let mut lexer = Lexer::new(&String::from(string));
-        lexer.next();
+    if let Ok(paths) = fs::read_dir(args.file.clone()) {
+        for path in paths {
+            compiler.parse_file(&path.unwrap().path());
+        }
     } else {
-        println!("error reading file");
+        let file = args.file.clone();
+        compiler.parse_file(&Path::new(&file));
     }
+    
 }
