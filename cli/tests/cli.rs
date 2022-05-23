1481aa7287eb79a2e0186f279bde6fe76c9a837c
Taylor Holliday <wtholliday@gmail.com>
2022-05-22 21:41:12 -0700

    Update cli.rs

--- cli/tests/cli.rs
+++ cli/tests/cli.rs
@@ -1,6 +1,8 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
+use std::fs;
+use std::path::Path;
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,6 +12,24 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
          .failure()
          .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn cases() -> Result<(), Box<dyn std::error::Error>> {
+
+    if let Ok(entries) = fs::read_dir("../tests/cases/") {
+        for entry in entries {
+            let path = entry.unwrap().path();
+
+            let mut cmd = Command::cargo_bin("lyte")?;
+            cmd.arg(path.into_os_string().into_string().unwrap());
+            // cmd.assert().success();
+            println!("{:?}", cmd.output());
+
+        }
+    }
 
     Ok(())
 }
