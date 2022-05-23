bf1a5cb4c3977c1bbd8180c0bd5f59d83a80f764
Taylor Holliday <wtholliday@gmail.com>
2022-05-22 21:27:37 -0700

    work on testing

---
+++ cli/tests/cli.rs
@@ -0,0 +1,15 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("lyte")?;
+
+    cmd.arg("test/file/doesnt/exist");
+    // cmd.assert()
+    //      .failure()
+    //      .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
