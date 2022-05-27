cf79683d4ec7edfd2f8fde49b2fd44dc6dbf2a5c
Taylor Holliday <taylor@audulus.com>
2022-05-26 17:29:53 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,23 +1,4 @@
 #!/bin/env bash
 
-for file in tests/cases/*.lyte
-do
-    cd cli
-    cargo run -- ../$file >& /dev/null
-    result=$?
-
-    if grep -q "expect failure" "../$file" ; then
-        if [ $result -eq 0 ]; then
-            echo ❌ $file \(expected failure\)
-        else
-            echo ✅ $file \(expected failure\)
-        fi
-    else
-        if [ $result -eq 0 ]; then
-            echo ✅ $file
-        else
-            echo ❌ $file
-        fi
-    fi
-    cd ..
-done
+cd cli
+cargo run -- --test ../tests/cases/
