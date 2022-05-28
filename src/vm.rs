4aa4fd80a5c8b1e525b35a3229be176fcd4f6e79
Taylor Holliday <wtholliday@gmail.com>
2022-05-27 21:10:12 -0700

    Update vm.rs

--- src/vm.rs
+++ src/vm.rs
@@ -13,14 +13,27 @@ enum Inst {
     StoreFloatImm(Reg, usize),
 }
 
+// According to wasm3, continuation passing is faster because
+// the function call arguments are mapped to CPU registers.
+struct Op {
+    f: fn(code: &[Op], ip: usize, mem: &[u8], sp: usize, i: i32, f: f32),
+}
+
+fn read4(mem: &[u8], addr: usize) -> [u8; 4] {
+    assert!(addr+3 < mem.len());
+    [mem[addr], mem[addr+1], mem[addr+2], mem[addr+3]]
+}
+
+fn f_add(code: &[Op], ip: usize, mem: &[u8], sp: usize, i: i32, f: f32) {
+    let x = f32::from_ne_bytes(read4(mem, sp));
+    (code[ip+1].f)(code, ip+1, mem, sp, i, f + x);
+}
+
 /// On some platforms (iOS) we can't generate machine code, so 
 /// here's an attempt at a VM.
 struct Vm {
     code: Vec<Inst>,
     mem: Vec<u8>,
-    ireg: [i32; 256],
-    freg: [f32; 256],
-    ip: usize,
 }
 
 impl Vm {
@@ -38,26 +51,30 @@ impl Vm {
     
     fn run(&mut self) {
 
-        let inst = self.code[self.ip];
+        let mut ireg = [0 as i32; 256];
+        let mut freg = [0.0 as f32; 256];
+        let ip = 0 as usize;
+
+        let inst = self.code[ip];
 
         match inst {
             Inst::Iadd(dst, a, b) => {
-                self.ireg[dst as usize] = self.ireg[a as usize] + self.ireg[b as usize];
+                ireg[dst as usize] = ireg[a as usize] + ireg[b as usize];
             }
             Inst::Fadd(dst, a, b) => {
-                self.freg[dst as usize] = self.freg[a as usize] + self.freg[b as usize];
+                freg[dst as usize] = freg[a as usize] + freg[b as usize];
             }
             Inst::LoadIntImm(dst, addr) => {
-                self.ireg[dst as usize] = i32::from_ne_bytes(self.read4(addr))
+                ireg[dst as usize] = i32::from_ne_bytes(self.read4(addr))
             }
             Inst::StoreIntImm(dst, addr) => {
-                self.write4(addr,  self.ireg[dst as usize].to_ne_bytes());
+                self.write4(addr,  ireg[dst as usize].to_ne_bytes());
             }
             Inst::LoadFloatImm(dst, addr) => {
-                self.freg[dst as usize] = f32::from_ne_bytes(self.read4(addr))
+                freg[dst as usize] = f32::from_ne_bytes(self.read4(addr))
             }
             Inst::StoreFloatImm(dst, addr) => {
-                self.write4(addr,  self.freg[dst as usize].to_ne_bytes());
+                self.write4(addr,  freg[dst as usize].to_ne_bytes());
             }
         }
     }
