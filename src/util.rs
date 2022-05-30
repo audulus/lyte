970c3a0f7f6141b1366a106128ff1db5c2f2b1a5
Taylor Holliday <taylor@audulus.com>
2022-05-29 19:44:51 -0700

    Create util.rs

---
+++ src/util.rs
@@ -0,0 +1,13 @@
+
+pub fn read4(mem: &[u8], addr: usize) -> [u8; 4] {
+    assert!(addr + 3 < mem.len());
+    [mem[addr], mem[addr + 1], mem[addr + 2], mem[addr + 3]]
+}
+
+pub fn write4(mem: &mut [u8], addr: usize, word: [u8; 4]) {
+    assert!(addr + 3 < mem.len());
+    mem[addr] = word[0];
+    mem[addr + 1] = word[1];
+    mem[addr + 2] = word[2];
+    mem[addr + 3] = word[3];
+}
