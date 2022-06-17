30908dbe4affc91c6f53f2819de40287fb1851f7
Taylor Holliday <taylor@audulus.com>
2022-06-17 08:04:25 -0700

    Update cli.rs

--- cli/tests/cli.rs
+++ cli/tests/cli.rs
@@ -1,37 +1,4 @@
-use assert_cmd::prelude::*; // Add methods on commands
 use goldentests::{TestConfig, TestResult};
-use predicates::prelude::*; // Used for writing assertions
-use std::fs;
-use std::path::Path;
-use std::process::Command; // Run programs
-
-#[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("lyte")?;
-
-    cmd.arg("test/file/doesnt/exist");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("could not read file"));
-
-    Ok(())
-}
-
-#[test]
-fn cases() -> Result<(), Box<dyn std::error::Error>> {
-    if let Ok(entries) = fs::read_dir("../tests/cases/") {
-        for entry in entries {
-            let path = entry.unwrap().path();
-
-            let mut cmd = Command::cargo_bin("lyte")?;
-            cmd.arg(path.into_os_string().into_string().unwrap());
-            // cmd.assert().success();
-            println!("{:?}", cmd.output());
-        }
-    }
-
-    Ok(())
-}
 
 #[test]
 fn run_golden_tests() -> TestResult<()> {
