33465c36e8667f7bb8ea8df083a6e488f9e85206
Taylor Holliday <wtholliday@gmail.com>
2023-02-13 22:12:49 -0800

    Update jit.rs

--- src/jit.rs
+++ src/jit.rs
@@ -39,3 +39,48 @@ impl Default for JIT {
         }
     }
 }
+
+impl JIT {
+
+    /// Compile our IR into native code.
+    pub fn compile(&mut self, input: &BlockArena) -> Result<*const u8, String> {
+
+
+        let name = "main";
+
+        // Next, declare the function to jit. Functions must be declared
+        // before they can be called, or defined.
+        //
+        // TODO: This may be an area where the API should be streamlined; should
+        // we have a version of `declare_function` that automatically declares
+        // the function?
+        let id = self
+            .module
+            .declare_function(&name, Linkage::Export, &self.ctx.func.signature)
+            .map_err(|e| e.to_string())?;
+
+        // Define the function to jit. This finishes compilation, although
+        // there may be outstanding relocations to perform. Currently, jit
+        // cannot finish relocations until all functions to be called are
+        // defined. For this toy demo for now, we'll just finalize the
+        // function below.
+        self.module
+            .define_function(
+                id,
+                &mut self.ctx)
+            .map_err(|e| e.to_string())?;
+
+        // Now that compilation is finished, we can clear out the context state.
+        self.module.clear_context(&mut self.ctx);
+
+        // Finalize the functions which we just defined, which resolves any
+        // outstanding relocations (patching in addresses, now that they're
+        // available).
+        self.module.finalize_definitions();
+
+        // We can now retrieve a pointer to the machine code.
+        let code = self.module.get_finalized_function(id);
+
+        Ok(code)
+    }
+}
