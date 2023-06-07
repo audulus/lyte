31e9d3e78d8d0b26199b6f5dfcf7bce821de2786
Taylor Holliday <taylor@audulus.com>
2023-06-07 10:40:41 -0700

    add IndexInterval

--- src/array_checker.rs
+++ src/array_checker.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use std::ops::Range;
 
 pub struct ArrayError {
     pub location: Loc,
@@ -9,6 +10,19 @@ struct IndexConstraint {
     pub name: Name,
     pub max: Option<i64>,
     pub min: Option<i64>,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct IndexInterval {
+    pub min: i64,
+    pub max: i64,
+}
+
+fn enclose(a: IndexInterval, b: IndexInterval) -> IndexInterval {
+    IndexInterval {
+        min: a.min.min(b.min),
+        max: a.max.max(b.max),
+    }
 }
 
 /// Local variable declaration.
@@ -34,12 +48,13 @@ impl ArrayChecker {
         }
     }
 
-    fn check_expr(&mut self, expr: ExprID, decl: &FuncDecl, decls: &DeclTable) {
+    fn check_expr(&mut self, expr: ExprID, decl: &FuncDecl, decls: &DeclTable) -> IndexInterval {
         match &decl.arena[expr] {
             Expr::Let(name, init, _) => {
                 self.check_expr(*init, decl, decls);
                 let ty = decl.types[expr];
                 self.vars.push(Var { name: *name, ty });
+                IndexInterval { min: 0, max: 0 }
             }
             Expr::If(cond, then_expr, else_expr) => {
                 let initial_constraint_count = self.constraints.len();
@@ -77,16 +92,19 @@ impl ArrayChecker {
                     }
                 }
 
-                self.check_expr(*then_expr, decl, decls);
+                let mut r = self.check_expr(*then_expr, decl, decls);
 
                 if let Some(else_expr) = else_expr {
-                    self.check_expr(*else_expr, decl, decls);
+                    let else_r = self.check_expr(*else_expr, decl, decls);
+                    r = enclose(r, else_r);
                 }
 
                 // Pop off any constraints not alreayd invalidated by changing data.
                 while self.constraints.len() > initial_constraint_count {
                     self.constraints.pop();
                 }
+
+                r
             }
             _ => {
                 todo!()
