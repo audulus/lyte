dfd64c321a73df36faf00f060564e94330de2eba
Taylor Holliday <wtholliday@gmail.com>
2025-02-04 07:26:58 -0800

    fix

--- cli/tests/cli.rs
+++ cli/tests/cli.rs
@@ -2,6 +2,6 @@ use goldentests::{TestConfig, TestResult};
 
 #[test]
 fn run_golden_tests() -> TestResult<()> {
-    let config = TestConfig::new("target/debug/lyte", "../tests/cases", "// ")?;
+    let config = TestConfig::new("target/debug/lyte", "../tests/cases", "// ");
     config.run_tests()
 }
