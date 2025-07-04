7e2534754d0de3c62c3dbbee9e6f28eb25bc260a
Taylor Holliday <wtholliday@gmail.com>
2025-07-04 14:01:21 -0700

    cleanup

--- src/jit.rs
+++ src/jit.rs
@@ -538,15 +538,15 @@ impl<'a> FunctionTranslator<'a> {
                 let lhs = self.translate_expr(lhs_id, decl, decls);
                 let rhs = self.translate_expr(rhs_id, decl, decls);
                 let t = decl.types[lhs_id];
-                if *t == crate::types::Type::Int32
-                    || *t == crate::types::Type::Bool
-                    || *t == crate::types::Type::Int8
-                {
-                    self.builder.ins().icmp(IntCC::NotEqual, lhs, rhs)
-                } else if *t == crate::types::Type::Float32 || *t == crate::types::Type::Float64 {
-                    self.builder.ins().fcmp(FloatCC::NotEqual, lhs, rhs)
-                } else {
-                    todo!()
+
+                match *t {
+                    crate::types::Type::Bool | crate::types::Type::Int32 | crate::types::Type::UInt32 | crate::types::Type::Int8 | crate::types::Type::UInt8 => {
+                        self.builder.ins().icmp(IntCC::NotEqual, lhs, rhs)
+                    }
+                    crate::types::Type::Float32 | crate::types::Type::Float64 => {
+                        self.builder.ins().fcmp(FloatCC::NotEqual, lhs, rhs)
+                    }
+                    _ => todo!(),
                 }
             }
             Binop::Less => {
